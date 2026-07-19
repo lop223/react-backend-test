7de6f231cf04d56b3729152cb0a89b72255d27bf
lop223 <dolgijmaks3@gmail.com>
2026-07-19 21:37:45 +0300

    s

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
 import express from "express";
 import cors from "cors";
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
-
 const app = express();
 app.use(express.json());
 app.use(
