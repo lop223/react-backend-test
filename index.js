684347f4b7c0dd5a48030704bc9c85a2c0129af6
lop223 <dolgijmaks3@gmail.com>
2026-07-19 21:34:57 +0300

    a

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
+app.use(express.static("dist"));
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello Wolrld!</h1>");
